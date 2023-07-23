2cdfb59906a8cf0e5adbd07cdf56a808936760ee
Dilahara MMS <41099408+chapayev0@users.noreply.github.com>
2023-07-23 00:18:28 -0700

    change databas

--- library/config.php
+++ library/config.php
@@ -2,7 +2,7 @@
 $host="localhost";
 $uname="root";
 $pwd="";
-$db="hdkit";
+$db="itmart";
 
 $connection=mysql_connect($host,$uname,$pwd) or die("Could not connect".mysql_error());
 mysql_select_db($db)or die("Could not connect with database.".mysql_error()); 
